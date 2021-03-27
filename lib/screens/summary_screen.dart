db5f9aa96639869e9303955486daf1677adfcf57
skcheongbrian <skcheongbrian@gmail.com>
2021-03-27 15:09:28 +0800

    Updated UI for SummaryPage
    Changed heading on top to be Home as well as adding the necessary widgets to display data.

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fintech_folio/components/gradient_app_bar.dart';
 import 'package:fintech_folio/components/custom_buttom_navigation_bar.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class SummaryScreen extends StatefulWidget {
   static const String id = 'summary_screen';
@@ -14,8 +15,87 @@ class _SummaryScreenState extends State<SummaryScreen> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: GradientAppBar(title: "Summary"),
-      bottomNavigationBar: CustomBottomNavigationBar(screenId: SummaryScreen.id),
+      appBar: GradientAppBar(title: "Home"),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: [
+                  Text(
+                    'Welcome, Pei Ying',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                    ),
+                  ),
+                ],
+              ),
+              Card(
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Daily Expenses'),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text('dummy value'),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Expenses'),
+                    ),
+                    // For chart and keys
+                    Row(),
+                  ],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text('Recent Transactions'),
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: TextButton(
+                            child: Text('See all'),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => FinanceScreen()));
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar:
+          CustomBottomNavigationBar(screenId: SummaryScreen.id),
     );
   }
 }

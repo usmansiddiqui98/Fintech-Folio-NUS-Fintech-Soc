cbcbb5764f3b5099dc9a9265cad1fd0b5fda72f5
VimuthM <vimuthmendis@gmail.com>
2021-04-02 16:26:32 +0800

    Added finance screen structure

--- lib/screens/finance_screen.dart
+++ lib/screens/finance_screen.dart
@@ -1,7 +1,8 @@
+import 'package:fintech_folio/components/rounded_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fintech_folio/components/gradient_app_bar.dart';
 import 'package:fintech_folio/components/custom_buttom_navigation_bar.dart';
-
 
 class FinanceScreen extends StatefulWidget {
   static const String id = 'finance_screen';
@@ -11,11 +12,117 @@ class FinanceScreen extends StatefulWidget {
 }
 
 class _FinanceScreenState extends State<FinanceScreen> {
+
+  Widget today() {
+    return Align();
+  }
+
+  Widget weekly() {
+    return Align();
+  }
+
+  Widget monthly() {
+    return Align();
+  }
+
+  Widget _displayWidget;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GradientAppBar(title: "Finances"),
-      bottomNavigationBar: CustomBottomNavigationBar(screenId: FinanceScreen.id),
+        appBar: GradientAppBar(title: "Finances"),
+        bottomNavigationBar:
+            CustomBottomNavigationBar(screenId: FinanceScreen.id),
+        body: SafeArea(
+            child: Container(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: RoundedButton(
+                                  title: 'Today',
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    setState(() {
+                                      _displayWidget = today();
+                                    });
+                                  }
+                              ),
+                            ),
+                            Expanded(
+                              child: RoundedButton(
+                                  title: 'Weekly',
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    setState(() {
+                                      _displayWidget = weekly();
+                                    });
+                                  }
+                              ),
+                            ),
+                            SizedBox(),
+                            Expanded(
+                              child: RoundedButton(
+                                  title: 'Monthly',
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    setState(() {
+                                      _displayWidget = monthly();
+                                    });
+                                  }
+                              ),
+                            ),
+                          ]
+                      ),
+                      Card(
+                        color: Colors.white,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text('Expenses'),
+                            ),
+                            // For changing widget
+                            _displayWidget,
+                          ],
+                        ),
+                      ),
+                      Card(
+                        color: Colors.white,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text('Income'),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Text('dummy value'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        color: Colors.white,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text('Overall Balance'),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Text('dummy value'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+                )
+            )
+        )
     );
   }
 }
